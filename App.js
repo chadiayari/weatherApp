b94b7eff533c8aed1fbabcb92b939d397a1cae92
Chadi Ayari <chadi-98@live.fr>
2021-12-29 01:39:56 +0100

    Initial Commit
    Created Interface views and ScrollViews for temperature.

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, ScrollView, Dimensions } from "react-native";
+
+const SCREEN_WIDTH = Dimensions.get("window").width
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.main_container}>
+      <View style={styles.city}>
+        <Text style={styles.city_name}>Seouls</Text>
+      </View>
+
+      <ScrollView
+        horizontal 
+        pagingEnabled 
+        contentContainerStyle={styles.weather}
+        showsHorizontalScrollIndicator={false}
+      >
+        <View style={styles.weather_container}>
+          <Text style={styles.weather_degree}>27</Text>
+          <Text style={styles.weather_status}>Sunny</Text>
+        </View>
+        <View style={styles.weather_container}>
+          <Text style={styles.weather_degree}>27</Text>
+          <Text style={styles.weather_status}>Sunny</Text>
+        </View>
+        <View style={styles.weather_container}>
+          <Text style={styles.weather_degree}>27</Text>
+          <Text style={styles.weather_status}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main_container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "tomato",
+   },
+   city_name: {
+    fontSize: 68,
+    fontWeight: "bold",
+  },
+  city: {
+    flex: 1.2,
+    alignItems:"center",
+    justifyContent:"center"
+  },
+  weather: {
+  },
+  weather_container:{
+    alignItems:"center",
+    width: SCREEN_WIDTH
+  },
+  weather_degree:{
+    marginTop: 50,
+    fontSize:160
+  },
+  weather_status:{
+    marginTop: -20,
+    fontSize:29
   },
 });

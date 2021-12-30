1ceae27ea10368c1da15f1d6c97af30d9cafbae1
Chadi Ayari <chadi-98@live.fr>
2021-12-30 02:30:26 +0100

    UI improvements
    Added  icons depending on weather, changed theme color, changed font sizes, position and color.

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StyleSheet, View, Text, ScrollView, Dimensions, ActivityIndicator } from "react-native";
 import * as Location from 'expo-location';
 import React, {useEffect, useState} from "react"
+import { Fontisto } from '@expo/vector-icons'
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 const api_key = "784ab24ff2ed5d94d4288abed9e25d13";
+const icons = {
+  "Clouds": "cloudy",
+  "Atmosphere": "cloudy-gusts",
+  "Clear": "day-sunny",
+  "Snow": "snow",
+  "Rain": "rains",
+  "Thunderstorm": "lightning",
+  "Drizzle": "rain",
+}
 
 export default function App() {
-  const [city, setCity] = useState("Loading...");
+  const [city, setCity] = useState("Loading");
   const [days, setDays] = useState([]);
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState("");
@@ -26,7 +36,6 @@ export default function App() {
       const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${api_key}&units=metric`);
       const json = await response.json();
       setDays(json.daily)
-      console.log(json.daily)
     })();
   }, []);
 
@@ -40,7 +49,7 @@ export default function App() {
         horizontal 
         pagingEnabled 
         contentContainerStyle={styles.weather}
-        showsHorizontalScrollIndicator={true}
+        showsHorizontalScrollIndicator={false}
       >
         {days.length === 0 ? (
           <View style={styles.weather_container}>
@@ -49,12 +58,17 @@ export default function App() {
           ) : (
             days.map((day, index) => 
             <View key={index} style={styles.weather_container}>
-              <Text style={styles.weather_degree}>
-                {parseFloat(day.temp.day).toFixed(1)}°
-              </Text>
+              <View style={styles.temp_icon}>
+                <Text style={styles.weather_degree}>
+                  {parseFloat(day.temp.day).toFixed(1)}°
+                </Text>
+                <Fontisto name={icons[day.weather[0].main]} size={44} color="white" />
+              </View>
+                
               <Text style={styles.weather_status}>
                 {day.weather[0].main}
               </Text>
+
               <Text style={styles.description}>
                 {day.weather[0].description}
               </Text>
@@ -69,33 +83,42 @@ export default function App() {
 const styles = StyleSheet.create({
   main_container: {
     flex: 1,
-    backgroundColor: "tomato",
+    backgroundColor:"tomato"
    },
    city_name: {
     fontSize: 68,
+    color:"white"
   },
   city: {
-    flex: 1.2,
+    flex: 1,
     alignItems:"center",
-    justifyContent:"center"
+    justifyContent:"center",
   },
   weather: {
   },
   weather_container:{
+    width: SCREEN_WIDTH,
+  },
+  temp_icon:{
+    flexDirection:"row",
     alignItems:"center",
-    width: SCREEN_WIDTH
+    justifyContent:"space-around"
   },
   weather_degree:{
     marginTop: 20,
-    fontSize:140,
+    fontSize:110,
     fontWeight: "bold",
+    color:"white"
   },
   weather_status:{
-    marginTop: -5,
-    fontSize:49
+    textAlign:"center",
+    marginTop: 5,
+    fontSize:40,
+    color:"white"
   },
   description:{
-    marginTop: 5,
-    fontSize:22
+    textAlign:"center",
+    fontSize:20,
+    color:"white"
   },
 });
